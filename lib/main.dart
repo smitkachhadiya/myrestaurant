a90a431de266841a3c43162858f15238d643ae1a
smitkachhadiya <114665787+smitkachhadiya@users.noreply.github.com>
2023-01-25 22:43:56 +0530

    committed successfully

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,23 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:restaurant/loginpage.dart';
-import 'package:intl/intl.dart';
-import 'package:restaurant/register.dart';
 
 void main() {
   runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: loginpage()));
+      home: splashScreen()));
 }
 
+class splashScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+        splash: Scaffold(
+          body: Center(
+            child: LoadingAnimationWidget.staggeredDotsWave(color: Colors.black, size: 100),
+          ),
+        ),
+        nextScreen: loginpage());
+  }
+}
